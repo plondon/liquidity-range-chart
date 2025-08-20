9079b5b529ff852ea642f64f255b855f6a7a8247
plondon <philip.n.london@gmail.com>
2025-08-20 08:29:19 -0400

    Prevent zoom less than 1

--- src/LiquidityRangeChart/constants.ts
+++ src/LiquidityRangeChart/constants.ts
@@ -71,7 +71,7 @@ export const ANIMATION = {
 // Chart Behavior Constants
 export const CHART_BEHAVIOR = {
   // Zoom constraints
-  MIN_ZOOM: 0.1,
+  MIN_ZOOM: 1.0,
   MAX_ZOOM: 10,
   ZOOM_STEP: 0.2,
   
